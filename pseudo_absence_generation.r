cadfd27c3b9f29a4ebfe58c56ade16104376d0ca
Christopher Barnard <125367266+ChrisTheFish96@users.noreply.github.com>
2023-11-09 15:03:28 +0200

    Adding cc_sea cleaning for pseudo absence data

--- pseudo_absence_generation.r
+++ pseudo_absence_generation.r
@@ -10,7 +10,6 @@ library(sp)
 num_absence_points <- 20000
 
 # Import presence data in csv file
-# File is created by GBIF_collection_and_cleaning.r
 presence_data <- read.csv("CLEANED_S_frugiperda_data.csv")
 
 # Specify which columns contain latitude and longitude coordinates
@@ -25,25 +24,12 @@ presence_longitude <- presence_data[, longitude_column]
 absence_latitude <- runif(num_absence_points, -200, 200)
 absence_longitude <- runif(num_absence_points, -170, 180)
 
-# Create SpatialPoints objects for absence and presence coordinates
-absence_coords <- SpatialPoints(cbind(absence_longitude, absence_latitude))
-presence_coords <- SpatialPoints(cbind(presence_longitude, presence_latitude))
-
-# Identify which absence coordinates are not in presence coordinates
-# Filter absence coordinates based on the condition
-# Create a SpatialPointsDataFrame for the absence data
-not_in_presence <- !is.na(sp::over(absence_coords, presence_coords))
-absence_coords <- absence_coords[!not_in_presence, ]
-absence_data <- SpatialPointsDataFrame(coords = absence_coords, data = data.frame(Presence = rep(0, length(absence_coords))))
-
-# Extract coordinates as a matrix
-coord_matrix <- coordinates(absence_data)
-
-# Create new columns for latitude and longitude
-# Remove the original "coordinates" column
-absence_data$Latitude <- coord_matrix[, 1]
-absence_data$Longitude <- coord_matrix[, 2]
-absence_data$coordinates <- NULL
+# Create absence data frame
+absence_data <- data.frame(
+  Presence = rep(0, num_absence_points),
+  Latitude = absence_latitude,
+  Longitude = absence_longitude
+)
 
 # Write the data frame to a CSV file
 csv_file_path <- "absence_data.csv"
