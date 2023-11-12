dfb611c47769ee82dcdc3bfe5994b72c488d8996
Christopher Barnard <125367266+ChrisTheFish96@users.noreply.github.com>
2023-11-12 12:11:21 +0200

    Created prediction csv files for each ssp classification using RFR model

---
+++ future_predictions.py
@@ -0,0 +1,36 @@
+# Import necessary libraries and functions
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+# Read the CSV data and select relevant columns
+data = pd.read_csv('future_combined_data/2040/2040_ssp585_combined_data.csv') 
+selected_columns = ['Longitude', 'Latitude',"bio1","bio2","bio3","bio4","bio5","bio6","bio7","bio8","bio9","bio10","bio11","bio12","bio13","bio14","bio15","bio16","bio17","bio18","bio19"]
+data = data[selected_columns]
+
+X = data[['Longitude', 'Latitude', "bio1","bio2","bio3","bio4","bio5","bio6","bio7","bio8","bio9","bio10","bio11","bio12","bio13","bio14","bio15","bio16","bio17","bio18","bio19"]]
+
+# Select the relevant columns
+# Separate the environmental features from latitude and longitude
+# Standardize the environmental features
+# Combine the scaled environmental features with latitude and longitude
+bios = X[["bio1","bio2","bio3","bio4","bio5","bio6","bio7","bio8","bio9","bio10","bio11","bio12","bio13","bio14","bio15","bio16","bio17","bio18","bio19"]]
+lat_lon = X[['Longitude', 'Latitude']]
+scaler_bio = StandardScaler()
+bios_scaled = scaler_bio.fit_transform(bios)
+X_scaled = pd.concat([lat_lon, pd.DataFrame(bios_scaled, columns=bios.columns)], axis=1)
+
+# Load the trained model
+filename = 'Sf_rfr_model.sav'
+rfr_model = joblib.load(filename)
+
+predictions = rfr_model.predict(X_scaled)
+
+# Create a DataFrame with longitude, latitude, and presence columns
+result_df = pd.DataFrame({'longitude': data['Longitude'], 'latitude': data['Latitude'], 'presence': predictions})
+
+# Write the DataFrame to a CSV file
+result_df.to_csv('future_combined_data/2040/2040_ssp585_predictions.csv', index=False)
